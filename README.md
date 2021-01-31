bca03903ddb7c3c271b41b28d1bd7fab66c3e782
jay <jayaram.hariharan@utexas.edu>
2021-01-31 17:34:42 -0500

    switch lists to txt file

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # crossThePond
-Android app for US English <-> UK English translations.
+Android app for translating US English <-> UK English phrases.
 
 More or less a way for me to learn/practice Android app development using Kotlin and Android Studio.
 
 <br>
 
-*Demo of current prototype (as of 12/31/2020):*
+*Demo of current prototype:*
 
 <p align='center'>
-  <img src="https://github.com/elbeejay/crossThePond/blob/master/app_demo_31122020.gif" width="300" />
+  <img src="https://github.com/elbeejay/crossThePond/blob/master/app_demo.gif" width="300" />
 </p>
+
+
+### Contributing phrases
+
+If you have phrases you'd like to see become part of the app, please feel free
+to contribute.
+Currently, the paired list of phrases used in the app are available in the
+[key.txt](app/src/main/assets/key.txt).
+These phrases are organized as `<US Phrase>, <UK Phrase>`.
+After adding your phrases, please open a PR and we can get 'em into the app!
+
+For more general contributions/development please just comment your code and
+explain what you've done in the associated PR. 
--- app/src/main/java/com/example/crossthepond/MainActivity.kt
+++ app/src/main/java/com/example/crossthepond/MainActivity.kt
@@ -10,22 +10,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val usPhrases = listOf("Snow", "Six-Thirty", "Tap Water", "Thanks", "Sweater", "Pants", "Arugula")
+        // Load text file into memory once
+        val inputString = application.assets.open("key.txt").bufferedReader().use { it.readText() }
 
-        val ukPhrases = listOf("White Rain", "Half-Six", "Still Water", "Cheers", "Jumper", "Trousers", "Rocket")
+        val phrasePair: List<String> = inputString.split("\n")
+        val phraseSize = phrasePair.size - 2
 
-        val numPhrases = usPhrases.size - 1
-
+        // Make button and on-click, pull random pair of words from text phrases
         val genButton: Button = findViewById(R.id.button)
         genButton.setOnClickListener {
 
-            val phraseIndex = (0..numPhrases).random()
+            // Generate random value to pull from list
+            val randomVal = (0..phraseSize).random()
 
+            // Pull paired phrases based on random value
+            val randomPhrase = phrasePair[randomVal]
+
+            // Split paired phrase into a list of its own
+            val phraseList = randomPhrase.split(",")
+
+            // Stick first phrase (US phrase) into the US text view
             val usResultTextView: TextView = findViewById(R.id.textView2)
-            usResultTextView.text = "US: " + usPhrases[phraseIndex]
+            usResultTextView.text = "US: " + phraseList[0]
 
+            // Stick second phrase (UK phrase) into the US text view
             val ukResultTextView: TextView = findViewById(R.id.textView3)
-            ukResultTextView.text = "UK: " + ukPhrases[phraseIndex]
+            ukResultTextView.text = "UK: " + phraseList[1]
         }
     }
 }
